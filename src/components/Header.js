182c174aa067d24ca019b0b463914fb673a86d5a
RishabhDwivedi2 <90751717+RishabhDwivedi2@users.noreply.github.com>
2022-10-21 09:07:22 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ export default function Header()
     return(
         <div className="navbar">
             <nav>
-                <img className="logo" src="../images/meme.png"></img>
+                <img className="logo" src="./images/meme.png"></img>
                 <h4>meme generator</h4>
             </nav>
         </div>
